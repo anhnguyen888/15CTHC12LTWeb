a328c34175e3a629c0a27fe0a4ad268d87b52744
dinhanh <nd.anh@hutech.edu.vn>
2017-09-15 12:43:08 +0700

    Add project files.

---
+++ 15CTHC12LTWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_15CTHC12LTWeb.Startup))]
+namespace _15CTHC12LTWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
