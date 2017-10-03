781a3a3e05a1e79b60ae1274764bb37a424dfe80
dinhanh <nd.anh@hutech.edu.vn>
2017-10-03 13:27:09 +0700

    modify CoursesController

--- LTWeb15CTH12Advanced/Controllers/CoursesController.cs
+++ LTWeb15CTH12Advanced/Controllers/CoursesController.cs
@@ -14,7 +14,8 @@ namespace LTWeb15CTH12Advanced.Controllers
             dbContext = new ApplicationDbContext();
         }
         [Authorize(Roles = "GiangVien")]
-        // Create Course
+        // Create anh nguyen 888 Course
+        // Add new anh nguyen 888
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel()
